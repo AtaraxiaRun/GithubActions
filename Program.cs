6e65b0654579f432d39c8edeec15eaf91c0df43d
Wangrui <12988775182@qq.com>
2024-07-17 21:40:53 +0800

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿namespace AspNetCore.GithubActions.Console
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Hello, World!");
+            System.Console.Read();
+        }
+    }
+}
